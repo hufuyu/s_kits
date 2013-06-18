1e2e68cb21d29adbd142c2101534408a4bb3a4fd
hufuyu <hufuyu@gmail.com>
2013-06-18 12:12:03 +0800

    Update douban_fm_cli.py

--- douban_fm_cli.py
+++ douban_fm_cli.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python
 # coding: utf-8
 
-#+++++++++++++++++++++++++++++++++++++++++++++++++++++
 #
-#   1. add  private fm
-#   2. add  cache
-#   3. signal  system [ paulse \ next \ favorite \ drop ]
-#-----------------------------------------------------
-
-
+#  1. private  fm
+#  2. cache
+#  3. signal  [ paulse \ next \ favorite \ drop]
+#
+ 
+ 
  import httplib
  import json
  import os
@@ -16,34 +15,29 @@
  import subprocess
  import time
  
- reload(sys)
- sys.setdefaultencoding('utf-8')
+ 
+ class DoubanFM():
+     
+     
+ 
  
  while True:
-     # 获取播放列表
+     # get list
      httpConnection = httplib.HTTPConnection('douban.fm')
      httpConnection.request('GET', '/j/mine/playlist?type=n&channel=0')
      song = json.loads(httpConnection.getresponse().read())['song']
  
      picture = 'images/' + song[0]['picture'].split('/')[4]
  
-     # 下载专辑封面
-     if not os.path.exists(picture):
-         subprocess.call([
-             'wget',
-             '-P',
-             'images',
-             song[0]['picture']])
+     # get picture
+
  
-     # 发送桌面通知
-     subprocess.call([
-         'notify-send',
-         '-i',
-         os.getcwd() + '/' + picture,
-         song[0]['title'],
-         song[0]['artist'] + '\n' + song[0]['albumtitle']])
  
-     # 播放
+     # Play
      player = subprocess.Popen(['mplayer', song[0]['url']])
      time.sleep(song[0]['length'])
      player.kill()
+     
+     
+if __name__ == '__main__':
+     play = DoubanFM() 

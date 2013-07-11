1c33ee727a2679df5218eae808b90c31b67a69e5
hufuyu <hufuyu@gmail.com>
2013-07-11 09:37:52 +0800

    add jijia2

--- chk_info_gae/admin.py
+++ chk_info_gae/admin.py
@@ -9,13 +9,36 @@
 #---
 
 import datetime
+import os
+
+import jinja2
+import webapp2
 
 from google.appengine.ext import db
 from google.appengine.api import users
+
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'])
 
 class adminHandler(webapp.RequestHandler):
 
     def get(self):
         pass
+
+
+
+'''
+        template_values = {
+            'greetings': greetings,
+            'guestbook_name': urllib.quote_plus(guestbook_name),
+            'url': url,
+            'url_linktext': url_linktext,
+        }
+
+        template = JINJA_ENVIRONMENT.get_template('index.html')
+        self.response.write(template.render(template_values))
+'''
+
 
 

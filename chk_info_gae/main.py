c221eab3fc7636973f4f41ea9029fde5084a59f9
hufuyu <hufuyu@gmail.com>
2013-07-11 09:38:13 +0800

    update

--- chk_info_gae/main.py
+++ chk_info_gae/main.py
@@ -15,8 +15,8 @@
 import logging
 import re
 import webapp2 as webapp
-
-import appDB
+import datetime
+import models
 
 from google.appengine.api import mail
 from google.appengine.api import urlfetch
@@ -30,21 +30,34 @@ from xml.dom.minidom import parseString
 class indexHandler(webapp.RequestHandler):
 
     def get(self):
-        user = users.get_current_user()
-        if user:
-            logging.info(user.email())
-        else:
-            logging.info('No User!')
+        index_tpl = """
+<html><head>
+<title>%s</title>
+<style>
+body {padding: 20px;font-family: arial, sans-serif;font-size: 14px;}
+pre { background: #F2F2F2; padding: 10px; }
+</style></head>
+<body>%s
+<pre>%s</pre>
+%s</body></html>
+"""
+        title = "chk-info-gae:check some info in sometime,powed by gae."
+        content = "<h1>chk-info-gae</h1><br/>"
+        pre_content = "this is system running info."
+        footer = "powered by gae, Licence:BSD , Other Question,PLs Mail:hufuyu@gmail.com"
+        index_tpl_args = (title,content,pre_content,footer)
+        self.response.headers['Content-Type'] = 'text/html'
+        self.response.out.write(index_tpl % index_tpl_args)
 
 class setupHandler(webapp.RequestHandler):
 
   def get(self):
-        chk_conf = models.CheckCfg.all()
+        chk_conf = models.CheckConfig.all()
         if not chk_conf.get():
             try:
-                ck_wooyun = models.CheckCfg(name ='test',site_type='wooyun_submit',
+                ck_wooyun = models.CheckConfig(name ='test',site_type='wooyun_submit',
                                         last_chk_id = 0,notice=True,mail_to='natthun@gmail.com',
-                                        last_get_status='200',last_chk_id=0)
+                                        )
                 ck_wooyun.put()
                 res_wooyun = models.ResPool(site_type='wooyun_submit',url='www.wooyun.org/feeds/submit',
                                             last_get_status='200',last_save_id=0)
@@ -63,35 +76,56 @@ class setupHandler(webapp.RequestHandler):
 class chkHandler(webapp.RequestHandler):
 
     def get(self):
-        appcfg = self._chkReqArgs()
-        if not appcfg:
-            self.response.headers['Content-Type'] = 'text/plain'
-            self.response.out.write("bad request or other reason!")
+        chk_cfg = self._chkReqArgs()
+        if not chk_cfg:
+            self.error(404)
             return
 
-        if appcfg.site_type == 'wooyun_submit':
-            msgs = chkWooyunSubmitRSS(appcfg.url,appcfg.key_words).fetchInfo()
+        res_pool = models.ResPool.all().filter('site_type =',chk_cfg.site_type).get()
+
+        if not res_pool:
+            logging.error('site_type not set in ResPool,Pls chk < %s >' % chk_cfg.site_type)
+            self.error(404)
+            return
+
+        for res in res_pool:
+            # check gap, if need fetch data
+
+            # fetch data  and save
+            if res.site_type == 'wooyun_submit':
+                chk_wooyun = chkWooyunSubmitRSS(chk_cfg.url,chk_cfg.key_words)
+                msgs = chk_wooyun.fetchInfo()
+            # save data: get last saved data.  delete repeat.
+            #getData('WooyunSubmitData',res.last_save_id)
+            #    chk_wooyun.saveData(msgs,res.last_save_id + 1)
+            #    res.last_save_id += 1
+            #    res.put()
+            # chk data which contain keyword
+
+
         if appcfg.notice and msgs:
             logging.info("prepare send notice ...")
-            self.sendNotice('natthun@gmail.com','Wooyun',msgs)
+            self.sendNotice(chk_cfg.mail_to,'Wooyun',msgs)
             self.response.headers['Content-Type'] = 'text/plain'
             self.response.out.write("mail send!")
 
     def _chkReqArgs(self):
-        sysconf = models.AppConfig.all()
-        # '/chk/(?P<name>)',check name in table models.AppConfig.name
-        if not sysconf.get():
-            self.response.headers['Content-Type'] = 'text/plain'
-            self.response.out.write("Nothing to be check,Pls set frist!")
+        chk_conf = models.CheckConfig.all()
+        # '/chk/(?P<name>)',check name in table models.CheckConfig.name
+        if not chk_conf.get():
+            logging.info('Not set CheckConfig item,Pls set it first')
             return
         arg = self.request.path[5:]
-        for entity in sysconf.run():
+        for entity in chk_conf.run():
             if arg == entity.name:
                 return entity
         logging.info("Bad Request Url:" + self.request.path)
         #
         #self.redirect("/404.html")
         self.error(404)
+
+    def checkKeyWord(self,msgs):
+        pass
 
     def sendNotice(self,mailto,site,msgs):
         sender_address = "Support <hufuyu@gmail.com>"
@@ -143,9 +177,7 @@ class chkWooyunSubmitRSS():
             logging.info("fetch info status OK.")
             rss_dom = parseString(req.content)
             items = self._parseWooyunRss(rss_dom)
-            self.saveItem(items)
-            self.notice_msgs = self.checkKeyWord(items)
-            return self.notice_msgs
+            return items
 
     def _parseWooyunRss(self,dom):
         items = dom.getElementsByTagName('item')
@@ -168,37 +200,20 @@ class chkWooyunSubmitRSS():
         # pease description info in item.items  is dict.
         for dict in items:
             ss = dict['description']
-            p1 = '(?<=简要描述：</strong><br/>)(?P<desc>.*)(?=<br/><strong>详细).*'
-            p2 = '(?<=说明：</strong><br/>)(?P<status>.*)(?=<br/><br/>).*'
+            p1 = u'(?<=简要描述：</strong><br/>)(?P<desc>.*)(?=<br/><strong>详细).*'
+            p2 = u'(?<=说明：</strong><br/>)(?P<status>.*)(?=<br/><br/>).*'
             pn = re.compile(p1+p2,re.M|re.S)
             tmp = pn.search(ss)
             if tmp :
                 tmp_dict = tmp.groupdict()
                 for key in tmp_dict.keys():
                     dict[key] = tmp_dict[key]
-                logging.info("Info item format ...")
+                logging.info("wooyun rss description item format ...")
         #    print(ss)
         return items
 
-    def checkKeyWord(self,msgs):
-        # item dict
-        notice  = []
-        if not self.keyword:
-            return msgs
-        # change zh_CN charter (，)
-        keylist = self.keyword.replace('，',',').split(',')
-        for item in msgs:
-            flag = 0
-            for key in keylist:
-                # try: dict has key['desc']
-                if key in item['desc']:
-                    flag =1
-                    break
-            if flag:
-                notice.append(item)
-        return notice
 
-    def saveItem(self,msgs):
+    def saveData(self,msgs):
         for msg in msgs:
             guid = msg['guid'].rsplit('/',1)[1]
             if not msg.has_key('desc'):
@@ -210,6 +225,8 @@ class chkWooyunSubmitRSS():
         logging.info("Info store in  DB.")
 
 #********************* start WSGI  ******************************
+#urls =[]
+#urls.add('/', indexHandler)
 
 app = webapp.WSGIApplication([('/', indexHandler),
                               ('/chk/.*', chkHandler),
